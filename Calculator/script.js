cff79814f14ccd0948e53b45a6fbcfae1167508d
E-jane15 <janeebaimay@gmail.com>
2024-05-09 08:20:59 +0100

    completed calculator

---
+++ Calculator/script.js
@@ -0,0 +1,19 @@
+const output = document.querySelector('.displayInput');
+const buttons = document.querySelectorAll('.button');
+
+
+function display(input){
+    output.value+=input;
+
+}
+
+function result(){
+    try{
+      let solution = eval(output.value.replace(/^[/*]/g, ""));
+      output.value= eval(solution);
+    }catch(error){
+        output.value = 'Error';
+    }
+}
+
+
